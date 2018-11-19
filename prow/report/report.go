eb2bb935d4edf368974c2d31ac15ad4ba52a668b
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-11-19 14:55:03 +0000

    fix: trimming description to 140 characters

--- prow/report/report.go
+++ prow/report/report.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -55,12 +55,23 @@ func reportStatus(ghc GithubClient, pj kube.ProwJob, childDescription string) er
 		if contextState == kube.AbortedState {
 			contextState = kube.FailureState
 		}
+
+		// find a better way to do this, but using the current failed description fails GitHub validation as exceeds 140 chars
+		// example description:
+		// 'build step "build-step-unnamed-2" exited with code 2 (image: "docker-pullable://jenkinsxio/jenkins-go@sha256:9104890bc0fdbed7535bf707cabaf3ddcb512170f9298f0dbad715953847d0ce");
+		//	  for logs run: kubectl -n jx logs d4865a2d-c68a-11e8-a85e-0a580a142233-fsmh6
+		//	  -c build-step-unnamed-2'
+		description := pj.Status.Description
+		if len(description) > 140 && strings.IndexByte(description, '(') > 0{
+			description = description[:strings.IndexByte(description, '(')]
+		}
+
 		if err := ghc.CreateStatus(refs.Org, refs.Repo, refs.Pulls[0].SHA, github.Status{
 			// The state of the status. Can be one of error, failure, pending, or success.
 			// https://developer.github.com/v3/repos/statuses/#create-a-status
-			State:       string(contextState),
-			Description: pj.Status.Description,
-			Context:     pj.Spec.Context,
+			State:	     string(contextState),
+			Description: description,
+			Context:	 pj.Spec.Context,
 			TargetURL:   pj.Status.URL,
 		}); err != nil {
 			return err
